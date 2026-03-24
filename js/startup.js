219f87f565537fefd2616b5ca23ef9ab234dffc1
Toni_4819 <132385090+Toni4819@users.noreply.github.com>
2026-03-24 19:29:20 +0100

    removed app.js

--- js/startup.js
+++ js/startup.js
@@ -5,6 +5,42 @@ import { PeerManager } from "./peer/utils/PeerManager.js";
 import { attachPeerManagerCallbacks } from "./ui/chatpanel.js";
 import { loadContacts } from "./ui/contacts.js";
 import { loadProfile } from "./ui/profile.js";
+import { applyTranslations, i18nReady } from "./i18n.js";
+
+// 1) Enregistrer les handlers PeerJS
+import "./peer/handlers/audio.js";
+import "./peer/handlers/file.js";
+import "./peer/handlers/gif.js";
+import "./peer/handlers/image.js";
+import "./peer/handlers/link.js";
+import "./peer/handlers/parser.js";
+import "./peer/handlers/text.js";
+
+// chat handlers
+import { initTBcall } from "./ui/handlers/TBcall.js";
+import { initTBfile } from "./ui/handlers/TBfile.js";
+import { initTBvideo } from "./ui/handlers/TBvideo.js";
+
+// 2) Importer l’UI
+import { initChat } from "./ui/chat.js";
+import { showProfilePanel } from "./ui/chatpanel.js";
+import { renderSidebar } from "./ui/sidebar.js";
+
+import { handleURLParams } from "./url-handler.js";
+// 3) Fonction de démarrage UI
+function appStart() {
+  handleURLParams();
+  renderSidebar();
+  showProfilePanel();
+  initChat();
+  initTBfile();
+  initTBcall();
+  initTBvideo();
+  i18nReady.then(applyTranslations);
+}
+
+// 4) On l’expose globalement
+window.appStart = appStart;
 
 // 0) Charger IndexedDB + contacts + profil AVANT PeerJS
 await Database.init();
